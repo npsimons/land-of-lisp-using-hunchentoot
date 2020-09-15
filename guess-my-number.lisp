9da2380a1d8befb632184430c10a3b522f56db7b
Nathan Paul Simons <nathan@hardcorehackers.com>
2020-09-15 15:44:55 -0700

    Checkpointing.

--- guess-my-number.lisp
+++ guess-my-number.lisp
@@ -19,3 +19,34 @@
   (defparameter *small* 1)
   (defparameter *big* 100)
   (guess-my-number))
+
+(defun square (n)
+  (* n n))
+
+(defun my-length (list)
+  (if list
+      (1+ (my-length (cdr list)))
+      0))
+
+(defvar *number-was-odd* nil)
+
+(defun pudding-eater (person)
+  (cond ((eq person 'henry)
+         (setf *arch-enemy* 'stupd-lisp-alien)
+         '(curse you lisp alien - you ate my pudding))
+        ((eq person 'johnny)
+         (setf *arch-enemy* 'useless-old-johnny)
+         '(i hope you choked on my pudding johnny))
+        (t
+         '(why you eat my pudding stranger ?))))
+
+(defun pudding-eater (person)
+  (case person
+    ((henry)
+     (setf *arch-enemy* 'stupd-lisp-alien)
+     '(curse you lisp alien - you ate my pudding))
+    (('johnny)
+     (setf *arch-enemy* 'useless-old-johnny)
+     '(i hope you choked on my pudding johnny))
+    (otherwise
+     '(why you eat my pudding stranger ?))))
---
+++ text-game-engine.lisp
@@ -0,0 +1,2 @@
+(defparameter *nodes* '((living-room (you are in the living-room.
+                                      a wizard is snoring loudly on the couch.))))
